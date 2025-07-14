05e2c701e6b76e123c0427a8c8fa843c885d23d1
FujisanRealEstate <154996184+Imath20@users.noreply.github.com>
2025-07-14 12:23:38 +0300

    search result updated

--- src/components/pages/searchresults.jsx
+++ src/components/pages/searchresults.jsx
@@ -15,7 +15,7 @@ const SearchResults = () => {
     const allData = [
         {
             title: "Pendule",
-            path: "/Simulari/pendule",
+            path: "/resurse/pendule",
             keywords: [
                 "pendul", "pendule", "pendulum", "pendulare", "pendulare simpla", "pendulare simplă", "pendul simplu", "pendul matematic", "pendul fizic",
                 "pend", "pen", "pndul", "pendu", "pendl", "penduul", "pendull", "pendlum", "pendulm", "pendulum", "pendulum simplu",
@@ -25,7 +25,7 @@ const SearchResults = () => {
         },
         {
             title: "Unde",
-            path: "/Simulari/unde",
+            path: "/resurse/unde",
             keywords: [
                 "unda", "unde", "wave", "waves", "unda mecanica", "unda mecanică", "unda transversala", "unda transversală", "unda longitudinala", "unda longitudinală",
                 "oscilație", "oscilatii", "oscilatie", "oscillation", "oscillations", "vibrație", "vibratie", "vibratii", "vibrations", "vibra", "vibrat",
@@ -34,7 +34,7 @@ const SearchResults = () => {
         },
         {
             title: "Lissajous",
-            path: "/Simulari/lissajous",
+            path: "/resurse/lissajous",
             keywords: [
                 "lissajous", "figuri lissajous", "figura lissajous", "lissajou", "lisa", "liss", "figuri", "figura", "oscilloscope", "oscilloscop",
                 "lissajous figures", "lissajous figure", "lissajous sim", "lissajous simulation", "lissajous experimente", "lissajous physics",
@@ -43,7 +43,7 @@ const SearchResults = () => {
         },
         {
             title: "Seisme",
-            path: "/Simulari/seism",
+            path: "/resurse/seism",
             keywords: [
                 "seism", "seisme", "cutremur", "cutremure", "earthquake", "earthquakes", "seismic", "seismograf", "seismologie", "sei", "seis",
                 "seismology", "seismology", "seismology", "seismology", "seism experiment", "seism sim", "seism simulation", "seism fizica",
@@ -69,24 +69,13 @@ const SearchResults = () => {
         },
         {
             title: "Simulari",
-            path: "/Simulari",
+            path: "/simulari",
             keywords: [
                 "simulare", "simulari", "simulări", "simulare fizica", "simulare unda", "simulare pendul", "sim", "simul", "simulatie", "simulație",
                 "simulations", "simulation", "simulare experiment", "simulare physics", "simulare experimente", "simulare sim", "simulare simulare",
                 "simulări fizica", "simulări experiment", "simulări sim", "simulări simulare", "simulati", "simulati", "simular", "simulr"
             ]
         },
-         {
-            title: "Muie Ţie",
-            keywords: [
-               "muie", "muie ţie", "muie tie", "muie ție", "muie ţie", "muie tie", "muie", "muie muie", "muie muie ţie", "muie muie tie", "muie muie ție", "muie muie ţie",
-                "muie muie muie", "muie muie muie ţie", "muie muie muie tie", "muie muie muie ție", "muie muie muie ţie", "muie muie muie tie",
-                "muie muie muie muie", "muie muie muie muie ţie", "muie muie muie muie tie", "muie muie muie muie ție", "muie muie muie muie ţie", "muie muie muie muie tie",
-                "pula", "pula mea", "pula ta", "pula lui", "pula lor", "pula noastra", "pula voastra", "pula voastra mea",
-                "pula mea muie", "pula ta muie", "pula lui muie", "pula lor muie", "pula noastra muie", "pula voastra muie", "pula voastra mea muie",
-                "suge pula", "suge pula mea", "suge pula ta", "suge pula lui", "suge pula lor", "suge pula noastra", "suge pula voastra", "suge pula voastra mea",
-            ]
-        }
     ];
 
     const results = useMemo(
